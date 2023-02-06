4f097defa3c9562ada26d7c52cb2a204a62a2662
Nastassia Lahoiskaya <nastassia_lahoiskaya@epam.com>
2023-02-06 14:04:05 +0600

    add modified Task1

--- Task1.py
+++ Task1.py
@@ -16,23 +16,20 @@ while unsorted_list:
 print(sorted_list)  # console output
 
 # calculate average for even and odd numbers and print both average result in console
-numbers = sorted_list
-summ = 0
-count = 0
-for num in numbers:        # iterating each number in list
-    if num % 2 == 0:       # checking condition if number is even
-        summ = summ + num  # calculate sum for even numbers
-        count += 1         # counting for even numbers
-average = summ / count     # counting average for even numbers
-print("average for even numbers:", average)  # console output
 
-numbers = sorted_list
-summ = 0
-count = 0
-for num in numbers:        # iterating each number in list
-    if num % 2 != 0:       # checking condition if number is odd
-        summ = summ + num  # calculate sum for odd numbers
-        count += 1         # counting for odd numbers
-average = summ / count     # counting average for odd numbers
-print("average for odd numbers:", average)  # console output
+sum_even = 0
+count_even = 0
+sum_odd = 0
+count_odd = 0
+for num in sorted_list:        # iterating each number in list
+    if num % 2 == 0:       # checking condition if number is even
+        sum_even = sum_even + num  # calculate sum for even numbers
+        count_even += 1         # counting for even numbers
+    elif num % 2 != 0:       # checking condition if number is odd
+        sum_odd = sum_odd + num  # calculate sum for odd numbers
+        count_odd += 1         # counting for odd numbers
+average_even = sum_even / count_even
+average_odd = sum_odd / count_odd
+print("average for even numbers:", average_even)  # console output
+print("average for odd numbers:", average_odd)  # console output
 
